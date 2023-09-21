9337b450596c2c2b706ede8f8bd77785c11cd0d6
VergilX <abhinand.dmanoj@gmail.com>
2023-09-21 23:07:59 +0530

    Project 4 complete

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,8 +1,3 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
@@ -12,3 +7,74 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(START)
+    // Initalizing values
+    @SCREEN
+    D=A
+    @address
+    M=D
+    @KBD
+    D=M
+    // If pressed or lifted
+    @PRESS
+    D;JNE
+    @LIFT
+    D;JEQ
+
+    (PRESS)
+        @i
+        M=0
+
+        (LOOPA)
+            @i
+            D=M
+            @8192
+            D=D-A
+            @START
+            D;JEQ
+
+            @8192
+            D=D+A
+            D=D+1
+            @i
+            M=D
+
+            @address
+            A=M
+            M=-1
+            D=A
+            D=D+1
+            @address
+            M=D
+            @LOOPA
+            0;JMP
+
+
+    (LIFT)
+        @i
+        M=0
+
+        (LOOPB)
+            @i
+            D=M
+            @8192
+            D=D-A
+            @START
+            D;JEQ
+
+            @8192
+            D=D+A
+            D=D+1
+            @i
+            M=D
+
+            @address
+            A=M
+            M=0
+            D=A
+            D=D+1
+            @address
+            M=D
+            @LOOPB
+            0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,48 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
 // Put your code here.
+
+@ans
+M=0
+
+@i
+M=0
+
+(LOOP)
+    @R1
+    D=M
+    @i
+    D=D-M
+    @END
+    D;JLE
+
+    @ans
+    D=M
+    @R0
+    D=D+M
+
+    @ans
+    M=D
+
+    @i
+    M=M+1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @ans
+    D=M
+    @R2
+    M=D
+    @FIN
+    0;JMP
+
+(FIN)
+    0;JMP
